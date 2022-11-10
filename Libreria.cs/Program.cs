bc21475a415671b827e1e99b67958e9cbdba7444
Samuel <105205182+Samuel-96@users.noreply.github.com>
2022-11-10 16:59:06 +0100

    Add files via upload
    Atualização do Libreria com métodos para exibir Carta y Baraja
    
    Fuera coñas, he hecho un método MuestraCarta y otro MuestraBaraja (que utiliza el MuestraCarta) para solucionar y evitar el foreach de main, estuve investigando y por lo visto no nos deja hacer foreach de la baraja así que por eso opté por hacer los métodos mencionados. He arreglado un poco el toString() de Carta y ahora es un public String MuestraCarta, con un Switch que muestre la carta según el palo al que pertenezca PERO no he deducido cómo aumentar el contador para que cambie el valor, tengo algunas ideas pero mañana lo comentamos.
    
    TODO  pendiente de arreglar el método MuestraCarta (quizás llevando el Switch a MuestraCartas (de baraja) en lugar de dejarlo en la clase Carta)
                pendiente de añadir un método para que mezcle las cartas
                pendiente añadir otro método que extraiga una carta de la baraja.

---
+++ Libreria.cs/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using libreria;
+
+namespace libreria
+{
+    public enum ePalo
+    {
+        Treboles,
+        Corazones,
+        Diamantes,
+        Picas
+    }
+
+    public enum eValor
+    {
+        As = 1,
+        Dos = 2,
+        Tres = 3,
+        Cuatro = 4,
+        Cinco = 5,
+        Seis = 6,
+        Siete = 7,
+        Ocho = 8,
+        Nueve = 9,
+        Diez = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+
+    }
+
+    public class Carta
+    {
+        //atributos
+        private ePalo palo;
+        private eValor valor;
+        // Contructores
+        public Carta(ePalo palo, eValor valor)
+        {
+            this.palo = palo;
+            this.valor = valor;
+        }
+        public Carta()
+        {
+            
+        }
+
+        //métodos
+        public String MuestraCarta()
+        {
+            int cont = 1;
+            /*
+            char p = 'T';
+            int v = 4;
+
+            return "┌───────┐\n" + "│" + v + "      |\n" + "│       |\n" + "│       |\n" + "│       |\n" + "│      " + p + "|\n" + "└───────┘";*/
+            switch(palo)
+            {
+                case ePalo.Treboles:
+                    return "┌───────┐\n" + "│" + cont + "      |\n" + "│       |\n" + "│       |\n" + "│       |\n" + "│" + "Tréboles" + "|\n" + "└───────┘"; break;
+
+                case ePalo.Corazones:
+                    return "┌───────┐\n" + "│" + cont + "      |\n" + "│       |\n" + "│       |\n" + "│       |\n" + "│" + "Corazones" + "|\n" + "└───────┘"; break;
+
+                case ePalo.Diamantes:
+                    return "┌───────┐\n" + "│" + cont + "      |\n" + "│       |\n" + "│       |\n" + "│       |\n" + "│" + "Diamantes" + "|\n" + "└───────┘"; break;
+
+                case ePalo.Picas:
+                    return "┌───────┐\n" + "│" + cont + "      |\n" + "│       |\n" + "│       |\n" + "│       |\n" + "│  " + "Picas" + "|\n" + "└───────┘";
+            }
+            return "fin de la baraja";
+        }
+    } //fin de la clase Carta
+
+    public class Baraja
+    {
+        private List<Carta> baraja; //la sintaxis es List<Carta> baraja = new List<Carta>(tamaño);
+        
+        //constructores
+        public Baraja()
+        {
+            baraja = new List<Carta>(52);
+        }
+
+        //métodos
+        public void RellenaBaraja()
+        {
+            for (int p = 0; p < 4; p++)// Bucle de palos, 4 pasadas
+            {
+                for (int v = 1; v < 14; v++)// Bucle de cartas, 13 pasadas
+                {
+                    Carta c = new Carta((ePalo)p, (eValor)v); //creamos una carta 
+                    baraja.Add(c); //la añadimos a la List
+                }
+            }
+        }
+
+        public void MuestraCartas()
+        {
+            foreach(Carta c in baraja)
+            {
+                Console.WriteLine(c.MuestraCarta());   
+            }
+        }
+
+    } //fin de la clase Baraja
+
+    public class programa
+    {
+        static void Main(string[] args)
+        {
+            Baraja baraja = new Baraja();
+            baraja.RellenaBaraja();
+            baraja.MuestraCartas();
+        }
+    }
+}
